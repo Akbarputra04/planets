2d56aedd66f7e72df50428e506f3b1b927d20f1d
Akbarputra04 <ap975527@gmail.com>
2023-10-24 10:04:30 +0700

    add infinite scroll to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import FAB from "../../components/fab";
 import Card from "../../components/card";
 import Title from "../../components/title";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useEffect, useState } from 'react';
 
 export interface IPlanets {
   name: string,
@@ -21,25 +23,53 @@ export interface IPlanets {
   url: string
 }
 
+interface IResponse {
+  count: number,
+	next: string,
+	previous: string,
+	results: Array<IPlanets>
+}
+
 const Home = () => {
 
   const navigate = useNavigate()
 
+  const [page, setPage] = useState(1)
+  const [planets, setPlanets] = useState<IResponse>({count: 0, next: '', previous: '', results: []})
+
   const getPlanets = () => {
-    return fetch('https://swapi.dev/api/planets').then(res => res.json())
+    return fetch(`https://swapi.dev/api/planets/?page=${page}`).then(res => res.json())
   }
 
-  const { data, isLoading } = useQuery('getPlanets', getPlanets)
+  const { data, isLoading } = useQuery(['getPlanets', page], getPlanets)
 
-  if (isLoading) return (<Card><Title>Loading...</Title></Card>)
+  useEffect(() => {
+    if (data) {
+      setPlanets({...data, results: [...planets?.results, ...data.results]})
+    }
+  }, [data])
+
+  if (isLoading && planets.results.length === 0) return (<Card><Title>Loading...</Title></Card>)
 
   return (
     <section>
-      {data.results.map((item: IPlanets) => (
-        <Card key={item.name} onClick={() => navigate("/detail", {state: {url: item.url}})}>
-          <Title>{item.name}</Title>
-        </Card>
-      ))}
+      <InfiniteScroll
+        dataLength={planets?.results?.length}
+        next={() => setPage(page+1)}
+        hasMore={!!planets?.next}
+        loader={<h4 style={{ textAlign: 'center' }}>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>End of data</b>
+          </p>
+        }
+      >
+        {planets?.results?.map((item: IPlanets) => (
+          <Card key={item.name} onClick={() => navigate("/detail", {state: {url: item.url}})}>
+            <Title>{item.name}</Title>
+          </Card>
+        ))}
+      </InfiniteScroll>
       <FAB />
     </section>
   )
